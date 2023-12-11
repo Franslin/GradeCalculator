2a867524df998c3bdd4e714b6e1276261e739d69
Frans Lindström <frans.lindstrom.webd23jon@edu.tucsweden.se>
2023-12-11 20:19:35 +0100

    Added comments to code

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ namespace Program
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //If the calculator should run or not
             bool runCalculator = false;
 
             Console.WriteLine("Vill du beräkna betyg för en elev? Ja/Nej");
             string answer = Console.ReadLine().ToLower();
             if (answer == "ja") {
-                runCalculator = true;
+                runCalculator = true; //Runs the calculator
             }
 
             while (runCalculator)
@@ -20,6 +21,7 @@ namespace Program
                 int score = Convert.ToInt32(Console.ReadLine());
                 string grade;
 
+                //Determind grade
                 if (score >= 90)
                     grade = "A";
                 else if (score >= 80 && score <= 89)
@@ -33,17 +35,18 @@ namespace Program
                 else
                     grade = "F";
 
-                Console.WriteLine($"Elevens betyg är: {grade}");
+                Console.WriteLine($"Elevens betyg är: {grade}"); //Prints out the grade
 
+                //If the program should continue running or not
                 Console.WriteLine("Vill du beräkna betyg för en till elev? Ja/Nej");
                 string answer2 = Console.ReadLine().ToLower();
                 if (answer2 == "ja")
                 {
-                    Console.Clear();
+                    Console.Clear(); //Clears console and then the loop restarts
                 }
                 else
                 {
-                    Console.WriteLine("Programmet är nu avslutat");
+                    Console.WriteLine("Programmet är nu avslutat"); //Program is canceled
                     runCalculator = false;
                 }
             }
